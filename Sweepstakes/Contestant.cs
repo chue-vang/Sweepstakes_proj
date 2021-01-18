61246bef2f299e0ec8d78c7113981a8ae6d90e33
unknown <chue.vang0@gmail.com>
2021-01-18 12:45:53 -0600

    Initial commit.

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        //member variables
+        public string firstName;
+        public string lastName;
+        public string email;
+        public string registrationNumber;
+
+
+
+        //constructor
+        public Contestant()
+        {
+
+        }
+
+
+
+        //member methods
+    }
+}
