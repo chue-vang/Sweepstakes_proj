c32d15e9ae08af58800803620e5612447a4d237c
unknown <chue.vang0@gmail.com>
2021-01-18 13:17:52 -0600

    Created the necessary classes.

---
+++ Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Sweepstakes
+    {
+        //member variables
+        public string name;
+
+
+
+        //constructor
+        public Sweepstakes(string name)
+        {
+
+        }
+
+
+
+
+        //member methods
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+
+        public Contestant PickWinner()
+        {
+
+        }
+
+        public void PringContestantInfo(Contestant contestant)
+        {
+
+        }
+    }
+}
