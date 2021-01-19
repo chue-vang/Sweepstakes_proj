f5aacaf6347de04a97de5d30a7ecbeb5734b9983
unknown <chue.vang0@gmail.com>
2021-01-19 14:02:44 -0600

    Implemented factory design patern in the Simulation class to allow a user to choose between a Stack or Queue manager.

--- Sweepstakes/Simulation.cs
+++ Sweepstakes/Simulation.cs
@@ -12,6 +12,21 @@ namespace Sweepstakes
         {
             //here you will call the factory method to create a manager, save it as a variable
             //Pass the created manager into a MarketingFirm instantiation
+            string userInput = UserInterface.GetUserInputFor("Select either a Stack manager or a Queue manager");
+            MarketingFirm marketingFirm = new MarketingFirm(ManageSweepstakesObject(userInput));
+        }
+
+        public ISweepstakesManager ManageSweepstakesObject(string userInput)
+        {
+            switch (userInput)
+            {
+                case "Stack":
+                    return new SweepstakesStackManager();
+                case "Queue":
+                    return new SweepstakesQueueManager();
+                default:
+                    throw new ApplicationException(string.Format("Not a valid selection."));
+            }
         }
     }
 }
